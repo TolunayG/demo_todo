18d28da6a41f89b10d37f3bddf3f428a53c8c654
align-els <aligng1620@gmail.com>
2018-06-19 11:22:20 +0300

    minor changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController
