e2a74b6de43a042e8d645136cc53bbc766b8bb21
align-els <aligng1620@gmail.com>
2018-06-19 14:31:32 +0300

    error response wrapper added

---
+++ src/main/java/com/example/demo/ErrorResponseWrapper.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class ErrorResponseWrapper
+{
+    private int code;
+    private String reason;
+
+    public ErrorResponseWrapper(int code, String reason) {
+        this.code = code;
+        this.reason = reason;
+    }
+
+    public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
