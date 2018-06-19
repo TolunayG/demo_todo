95f10523b309fb5f994e82374c3fdee93fa4f462
align-els <aligng1620@gmail.com>
2018-06-19 14:44:23 +0300

    getter setter for user enttity

--- src/main/java/com/example/demo/UserEntity.java
+++ src/main/java/com/example/demo/UserEntity.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class UserEntity
 {
     @Id
+    @GeneratedValue
     private Long id;
 
     @Column(name="email")
@@ -15,4 +17,13 @@ public class UserEntity
 
     @Column(name="password")
     private String password;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
