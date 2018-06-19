82576a931cc1e1cd2278df3dbe1d0fdde5355ea0
align-els <aligng1620@gmail.com>
2018-06-19 11:15:47 +0300

    Sprint Init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController
+{
+    @Autowired
+    private UserRepository repository;
+    
+    @GetMapping("/currency-exchange/from/{id}/to/{email}")
+    public String index(@PathVariable String id, @PathVariable String email) {
+        repository.save(new UserEntity(0L, "asd"));
+        return "" + repository.count();
+    }
+}
