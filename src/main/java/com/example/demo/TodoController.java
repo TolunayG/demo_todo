72d16544175f4788fddd7dd5f4fc4f9653744954
TolunayGedikoglu-Laptop <tolunaygedikoglu@gmail.com>
2018-06-20 11:11:36 +0300

    Todo controller and repository added

---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class TodoController
+{
+    
+}
