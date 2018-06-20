38b23cfddb751a270ba526126d46c8a971cdbbb5
align-els <aligng1620@gmail.com>
2018-06-20 08:11:39 +0300

    small refactoring

--- src/main/java/com/example/demo/TodoEntity.java
+++ src/main/java/com/example/demo/TodoEntity.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,7 +10,7 @@ public class TodoEntity
     @Id @GeneratedValue
     private Long id;
     private String text;
-    private boolean status;
+    private Boolean status;
     private Long creationDate;
     private Long dueDate;
 
@@ -21,7 +20,7 @@ public class TodoEntity
     public String getText() { return text; }
     public void setText(String text) { this.text = text; }
 
-    public boolean getStatus() { return status; }
+    public Boolean getStatus() { return status; }
     public void setStatus(Boolean status) { this.status = status; }
 
     public Long getCreationDate() { return creationDate; }
